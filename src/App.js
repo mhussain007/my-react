739407022146162ec24577b92f9a2ffeac152c48
Mohammad Hussain <mohammed.hussain007@gmail.com>
2023-12-22 12:04:11 +0530

    intial install

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Login from './Login';
+
+function App() {
+
+  return (
+    <center>
+    <h1>App</h1>
+      <Login />
+    </center>
+  )
+}
+export default App;
